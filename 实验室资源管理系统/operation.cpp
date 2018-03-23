6e374a95f2c8e7736ec5543c08149e4f726d5eb3
泽宇 甘 <2017202047@ruc.edu.cn>
2018-03-23 17:13:00 +0800

    blist~solved

--- 实验室资源管理系统/operation.cpp
+++ 实验室资源管理系统/operation.cpp
@@ -104,15 +104,33 @@ bool operation::DeleteResourse_E(int id)
 void operation::AddPerson(User &us)
 {
 	uList.push_front(us);
+
+	string username = us.getName();
+	ofstream SaveName("c:\\\\Resource\\\\USERS\\\\userlist.txt", ios::app);
+	SaveName << username << endl;
+
+	username += ".txt";
+	username = "c:\\\\Resource\\\\USERS\\\\" + username;
+	ofstream SaveUser(username, ios::ate);
+	/*将新添加的用户新建一个txt文件,存储在指定的目录下*/
+	SaveUser << us.getID() << endl;
 }
 
 bool operation::DeletePerson(const std::string & name)
 {
 	for (std::list<User>::iterator i = uList.begin(); i != uList.end(); ++i) {
 		if (i->getName() == name) {
-			uList.remove(*i);
+			uList.erase(i);
+			for (std::list<User>::iterator i = uList.begin(); i != uList.end(); ++i)
+			{
+				cout << "User:" << i->getName() << endl;
+			}
 			return true;
 		}
+	}
+	for (std::list<User>::iterator i = uList.begin(); i != uList.end(); ++i)
+	{
+		cout << "User:" << i->getName() << endl;
 	}
 	return false;
 }
@@ -123,7 +141,7 @@ bool operation::QueryBook(const std::string& name)
 	bool ret = false;
 
 	for (std::list<Book>::iterator i = bList.begin(); i != bList.end(); ++i) {
-		if ((*i).getName().find(name, 0) != std::string::npos||i->isInStorage())
+		if ((*i).getName().find(name, 0) != std::string::npos && i->isInStorage())
 		{
 			ret = true;
 			std::cout << i->getID() << ' ' << i->getName() << ' ' << i->getAuthorName()
@@ -138,7 +156,7 @@ bool operation::QueryEassy(const std::string &name)
 	bool ret = false;
 
 	for (std::list<Eassy>::iterator i = eList.begin(); i != eList.end(); ++i) {
-		if (i->getName().find(name, 0) != std::string::npos) {
+		if (i->getName().find(name, 0) != std::string::npos && i->isInStorage()) {
 			ret = true;
 			std::cout << i->getID() << ' ' << i->getName() << ' ' << i->getAuthorName() << std::endl;
 		}
@@ -206,23 +224,50 @@ void operation::MENU()
 	cout << "11、文献借出" << endl;
 	cout << "12、文献归还" << endl;
 	cout << "13、在线阅读" << endl;
+	cout << "14、退出" << endl;
 }
 
 
 operation::~operation() {
+	/*将bList重新写入文件*/
 	ofstream SaveBookName("c:\\\\Resource\\\\BOOKS\\\\booklist.txt", ios::trunc | ios::app);
 	for (std::list<Book>::iterator i = bList.begin(); i != bList.end(); ++i) {
 		SaveBookName << i->getName() << endl;
 	}
+	for (std::list<Book>::iterator i = bList.begin(); i != bList.end(); ++i) {
+		string name = i->getName();
+		name += ".txt";
+		ofstream SaveBook("c:\\\\Resource\\\\BOOKS\\\\" + name, ios::trunc | ios::app);
+		SaveBook << i->getID() << endl << i->isInStorage() << endl << i->getAuthorName() << endl
+			<< i->getPublisherName() << endl << i->getContent() << endl;
+	}
 
+	/*将eList重新写入文件*/
 	ofstream SaveEassyName("c:\\\\Resource\\\\EASSYS\\\\eassylist.txt", ios::trunc | ios::app);
 	for (std::list<Eassy>::iterator i = eList.begin(); i != eList.end(); ++i) {
 		SaveEassyName << i->getName() << endl;
 	}
+	for (std::list<Eassy>::iterator i = eList.begin(); i != eList.end(); ++i) {
+		string name = i->getName();
+		name += ".txt";
+		ofstream SaveEassy("c:\\\\Resource\\\\EASSYS\\\\" + name, ios::trunc | ios::app);
+		SaveEassy << i->getID() << endl << i->isInStorage() << endl 
+			<< i->getAuthorName() << endl << i->getContent() << endl;
+	}
 
-	ofstream SaveUserName("c:\\\\Resource\\\\USERS\\\\userlist.txt", ios::trunc | ios::app);
+	/*将uList重新写入文件*/
+	ofstream SaveUserName("c:\\\\Resource\\\\USERS\\\\userlist.txt", ios::ate);
+	bool flag = false;
 	for (std::list<User>::iterator i = uList.begin(); i != uList.end(); ++i) {
-		SaveUserName << i->getName() << endl;
+		if (flag) SaveUserName << endl;
+		flag = true;
+		SaveUserName << i->getName();
+	}
+	for (std::list<User>::iterator i = uList.begin(); i != uList.end(); ++i) {
+		string name = i->getName();
+		name += ".txt";
+		ofstream SaveUser("c:\\\\Resource\\\\USERS\\\\" + name, ios::ate);
+		SaveUser << i->getID() << endl;
 	}
 }
 
